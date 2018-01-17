a7d2effc1ffce9b81be9139de66e9294cddca727
Daniel Reed <daniel.reed998@gmail.com>
2018-01-16 18:23:01 -0600

    possibly fixed cart bug

--- client/components/ShoppingCart.js
+++ client/components/ShoppingCart.js
@@ -14,40 +14,45 @@ export class ShoppingCart extends Component {
     }
 
     render() {
-        if (this.props.cart){
-            return (
-                <div>
-                    <ul>
-                        {this.props.cart.lineItems &&
-                            this.props.cart.lineItems.map(lineItem => {
-                                return (
-                                    <li key={lineItem.id}>
-                                        <div>{lineItem.product.title}</div>
-                                        <div>
+        console.log('lineitems:', this.props.cart.lineItems)
+        if (this.props.cart.lineItems){
+            if (this.props.cart.lineItems[0]){
+                return (
+                    <div>
+                        <ul>
+                            {this.props.cart.lineItems &&
+                                this.props.cart.lineItems.map(lineItem => {
+                                    return (
+                                        <li key={lineItem.id}>
+                                            <div>{lineItem.product.title}</div>
+                                            <div>
+                                                <button onClick={() => {
+                                                    if (lineItem.quantity > 1){
+                                                        this.props.decrement(lineItem.orderId, lineItem.product)
+                                                    } else {
+                                                        this.props.removeLineItem(this.props.cart.id, lineItem.id)
+                                                    }
+                                                }}>-</button> {/*make this*/}
+                                                <span>{`Qty: ${lineItem.quantity}`}</span>
+                                                <button onClick={() => this.props.increment(lineItem.orderId, lineItem.product)}>+</button> {/*make this*/}
+                                            </div>
+                                            <div>{`Total cost: ${lineItem.totalPrice}`}</div>
                                             <button onClick={() => {
-                                                if (lineItem.quantity > 1){
-                                                    this.props.decrement(lineItem.orderId, lineItem.product)
-                                                } else {
-                                                    this.props.removeLineItem(this.props.cart.id, lineItem.id)
-                                                }
-                                            }}>-</button> {/*make this*/}
-                                            <span>{`Qty: ${lineItem.quantity}`}</span>
-                                            <button onClick={() => this.props.increment(lineItem.orderId, lineItem.product)}>+</button> {/*make this*/}
-                                        </div>
-                                        <div>{`Total cost: ${lineItem.totalPrice}`}</div>
-                                        <button onClick={() => {
-                                            this.props.removeLineItem(this.props.cart.id, lineItem.id)
-                                        }}>Remove Item</button>
-                                    </li>
-                                )
-                            }
-                        )}
-                    </ul>
-                    <Link to={'/orderReview'}>
-                        <button>Submit Order</button>
-                    </Link>
-                </div>
-            )
+                                                this.props.removeLineItem(this.props.cart.id, lineItem.id)
+                                            }}>Remove Item</button>
+                                        </li>
+                                    )
+                                }
+                            )}
+                        </ul>
+                        <Link to={'/orderReview'}>
+                            <button>Submit Order</button>
+                        </Link>
+                    </div>
+                )
+            } else {
+                return <h2>Your cart is empty!</h2>
+            }
         } else {
             return (
                 <h2>Your cart is empty!</h2>
